e11bd757ccd05d4990a92a9193865afa1eb55a6d
HectorParada06 <hector.parada06@inacapmail.cl>
2025-09-26 18:46:18 -0300

    Contactos arreglos

--- crud/contactos/models.py
+++ crud/contactos/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Contactos(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    cantidad = models.IntegerField()
+    telefono = models.CharField(max_length=20)
+    correo = models.EmailField()
+    direccion = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nombre
